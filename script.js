763f69bf9608bfeb4c70ff4baa9965c9dd6c0d46
Sneha Patil <sneha174.patil@gmail.com>
2025-07-20 21:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.project, .box');
+hiddenElements.forEach((el) => observer.observe(el));
